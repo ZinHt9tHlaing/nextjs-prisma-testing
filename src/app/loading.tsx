c4b60ee02026b4294e42bf7bfadd368cd8aa345d
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-12-03 23:21:19 +0630

    testing middelwares

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 const LoadingPage = () => {
   return (
     <div className="flex items-center justify-center h-screen gap-2">
-      <span className="border-2 border-t-white border-t-transparent animate-spin inline-block size-7 rounded-full"></span>
+      <span className="border-2 border-t-transparent animate-spin inline-block size-7 rounded-full"></span>
       <h1 className="text-xl font-medium animate-pulse">App is loading...</h1>
     </div>
   );
